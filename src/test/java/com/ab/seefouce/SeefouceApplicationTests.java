8403606ea8741d2c72f1588b3566a7c01dcdddda
yuan <yuan.com>
2024-05-22 11:09:12 +0800

    feat:add test moudle

---
+++ src/test/java/com/ab/seefouce/SeefouceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.seefouce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeefouceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

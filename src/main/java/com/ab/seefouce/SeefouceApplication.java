56807ae8b26a32a495176941af4d55eb366d2c93
yuan <yuan.com>
2024-05-22 11:07:10 +0800

    feat:initialize submit

---
+++ src/main/java/com/ab/seefouce/SeefouceApplication.java
@@ -0,0 +1,13 @@
+package com.ab.seefouce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeefouceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeefouceApplication.class, args);
+    }
+
+}
